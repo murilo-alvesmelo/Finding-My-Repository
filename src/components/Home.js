6f73701624c7199c90327c22500db1f2a12bb8f0
murilo-alvesmelo <murilo.melo72@gmail.com>
2023-06-06 22:19:31 -0300

    Criando componente header

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { StyleSheet, View, Text, Platform, TouchableOpacity, TextInput, FlatList, Image, Keyboard} from "react-native";
-import  Icon  from "react-native-vector-icons/Feather";
 import Cards from "./Cards";
 import axios from "axios";
 import * as Animatable from 'react-native-animatable';
+import Header from "./Header";
 
 
 export default function Home(props){
@@ -20,33 +20,15 @@ export default function Home(props){
         setName('')
         setRepos([])
     }
+
     return(
         <>    
-            <View style={styles.container}>
-                <View style={styles.containerFind}>
-                    <Icon name="github" size={40}/>
-                    <Text style={styles.headerText}>Finding My Repository</Text>
-                    <View style={styles.inputIcon}>
-                        <TextInput
-                            style={styles.input}
-                            value={name}
-                            onChangeText={setName}
-                            />
-                        <TouchableOpacity
-                            style={styles.icon}
-                            onPress={() => (searchGithub(name), Keyboard.dismiss())}
-                            >
-                            <Icon name="search" size={30}/>
-                        </TouchableOpacity>
-                        <TouchableOpacity
-                            style={styles.icon}
-                            onPress={clear}
-                            >
-                            <Icon name="trash" size={30}/>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </View>  
+            <Header
+                name={name}
+                isName={valor => setName(valor)}
+                isSearch={valor => searchGithub(valor)}
+                isClear={clear}
+            />
             {
                 repos ?
                 <FlatList
@@ -66,42 +48,6 @@ export default function Home(props){
 }
 
 const styles = StyleSheet.create({
-    container: {
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    containerFind: {
-        marginTop: Platform.OS == 'ios' ? 40 : 40,
-        height: 150,
-        width: '90%',
-        borderWidth: 2,
-        borderRadius: 20,
-        backgroundColor: '#9400d3',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    headerText :{
-        fontSize: 20,
-        fontWeight: 'bold'
-    },
-    inputIcon: {
-        flexDirection: "row",
-        alignItems: 'center',
-    },
-    input: {
-        textAlign: 'center',
-        marginTop: 10,
-        height: 30,
-        width: 150,
-        borderWidth: 1,
-        borderRadius: 10,
-        
-    },
-    icon: {
-        flexDirection: 'row',
-        marginHorizontal: 5, 
-        marginTop: 5
-    },
     image: {
         width: '100%',
         height: '50%',
